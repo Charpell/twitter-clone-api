252897905440f62b80c1d088470c330d64f6354b
Charpell <charpellumeh@gmail.com>
2018-06-03 08:36:35 +0100

    Login a User

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -13,6 +13,7 @@ export default {
     createTweet: TweetResolver.createTweet,
     updateTweet: TweetResolver.updateTweet,
     deleteTweet: TweetResolver.deleteTweet,
-    signup: UserResolver.signup
+    signup: UserResolver.signup,
+    login: UserResolver.login
   }
 }
